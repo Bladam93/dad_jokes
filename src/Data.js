6c4101a23a42be3ba182336e4d5263620841cb65
Bladam93 <106922100+Bladam93@users.noreply.github.com>
2023-12-10 13:01:13 +0100

    teszt adat feltöltés

--- src/Data.js
+++ src/Data.js
@@ -1,19 +1,63 @@
 const jokes = [
   {
     id: 1,
-    text: "Miért hallgatja a rendőr a hasáról a rádiót? \n ???? \n -Hogy róla szóljon a műsor"
+    text: "- Miért nem origamizott Dzsingisz Kán?\n- ???\n- Mert nem hajt a tatár."
   },
   {
     id: 2,
-    text: "- Mit énekel a molylepke a szekrényben? \n ??? \n - Eddablúzt..."
+    text: "- Mi történik a Mikulással ha karambolozik?\n- ???\n- Szánja bánja!"
   },
   {
     id: 3,
-    text: "- Mit csinál a béna origamis?\n  ??? \n - Mindig ugyanazt hajtogatja!"
+    text: "- Hogy hívják az ejtőernyős indiánt?\n- ???\n- Aaa... paccs."
   },
   {
     id: 4,
-    text: "-Tudod mit csinálok Zsuzsi\n  ??? \n - Nagyon szeretlek"
+    text: "- Miért terjedt el Magyarországon a póker?\n- ???\n- Mert nem tudták Kordában tartani!"
+  },
+  {
+    id: 5,
+    text: "- Mit csinál az asztronauta, ha megszomjazik?\n- ???\n- Űrkutat ás."
+  },
+  {
+    id: 6,
+    text: "- Melyik település felbontása a legjobb?\n- ???\n- Bonyhádé"
+  },
+  {
+    id: 7,
+    text: "- Hogy nevezik a templomok közötti hálózatot?\n- ???\n- pap-LAN"
+  },
+  {
+    id: 8,
+    text: "- Mit kapunk, ha egy kör alakú területre építünk kastélyt?\n- ???\n- Körtelekvárt"
+  },
+  {
+    id: 9,
+    text: "- Miért nem beszélnek egymással a gumimacik?\n- ???\n- Haribó"
+  },
+  {
+    id: 10,
+    text: "- Mi Robert Den foglalkozása?\n- ???\n- Robert Den Író"
+  },
+  {
+    id: 11,
+    text: "- Mit tesz a programozó, ha meg akarja ölni egy megrendelő?\n-???\n- Fejlesztve menekül."
+  },
+  {
+    id: 12,
+    text: "- Miért nem szereti a kannibál a komornyikot?\n- ???\n- Mert inas"
+  },
+  {
+    id: 13,
+    text: "- Mi az? Elöl, hátul vérzik, és 4 lába van ?\n- ???\n- Boci, boci tarka, se füle, se farka..."
+  },
+  {
+    id: 14,
+    text: "- Mit kell adni a hasmenéses orrszarvúnak?\n- ???\n- Utat."
+  },
+  {
+    id: 15,
+    text: "- Mi az, amit a disznók megállás nélkül esznek a vályúból?\n- ???\n- Szünetmentes táp."
   }
 ];
 
